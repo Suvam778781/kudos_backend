dc38027f3e9573191b11c1d2d4442614a3813d36
suvampanda <suvam.panda@devriser.com>
2023-07-10 14:37:05 +0530

    update verifyjwt and testing addlike endpoint (working fine)

--- utils/verifyJWT.js
+++ utils/verifyJWT.js
@@ -3,19 +3,19 @@
 const jwt=require("jsonwebtoken")
 require('dotenv').config()
 
-const verifyJwt=(token)=>{
-    jwt.verify(token, process.env.secret_key, (err, decodedToken) => {
+const verifyJwt = (token) => {
+    return new Promise((resolve, reject) => {
+      jwt.verify(token, process.env.secret_key, (err, decodedToken) => {
         if (err) {
-            console.log(err)
-          return { error: "Invalid token", err }
+          console.log(err);
+          reject("Invalid token");
+        } else {
+          console.log(decodedToken, "decode");
+          resolve(decodedToken);
         }
-        else{
-            console.log(decodedToken,"decode")
-            return{ decodedToken}
-        }
-  
       });
-}
+    });
+  };
 
 module.exports={
     verifyJwt
