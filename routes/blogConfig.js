e6c13b50ea88261e6826523feb6957e04a908c97
suvampanda <suvam.panda@devriser.com>
2023-07-27 23:41:41 +0530

    change post to get request in blog config

--- routes/blogConfig.js
+++ routes/blogConfig.js
@@ -9,7 +9,7 @@ const blogConfigRoute = express.Router();
 
 blogConfigRoute.post("/addcomment/:post_id", handleComment);
 blogConfigRoute.post("/addlike/:post_id", handleLike);
-blogConfigRoute.post("/getuserlikepost", handleUserLikedPosts);
-blogConfigRoute.post("/getallpost", handleAllPosts);
+blogConfigRoute.get("/getuserlikepost", handleUserLikedPosts);
+blogConfigRoute.get("/getallpost", handleAllPosts);
 
 module.exports = { blogConfigRoute };
